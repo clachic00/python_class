6ac96db5d797b9b7deff20ea0913a97652a4f72f
clachic00 <anajrwk2@gmail.com>
2025-11-28 17:45:40 +0900

    python second commit

---
+++ 15_module/lion_module.py
@@ -0,0 +1,8 @@
+def gorrilla():
+    return"크왕"
+def tiger():
+    return"어흥"
+def pig():
+    return"꿀꿀"
+
+lion = "사자"
---
+++ 16_package/ohgiraffers/team/teacher.py
@@ -0,0 +1,3 @@
+def pig():
+    return "꿀꿀"
+ohgiraffers = "국내최고 IT 전문교육 강사팀"
